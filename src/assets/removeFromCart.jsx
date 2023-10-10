01555a055d514b75a13a7f6f5a2ddd82d6dd008d
soumya <soumya.chakraborty@hoichoi.tv>
2023-10-10 11:49:29 +0530

    Data update

--- src/assets/removeFromCart.jsx
+++ src/assets/removeFromCart.jsx
@@ -9,8 +9,8 @@ const RemoveFromCart = () => {
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
-			<circle cx="12" cy="12" r="10" stroke="#1C274C" stroke-width="1.5" />
-			<path d="M15 12H9" stroke="#1C274C" strokeWidth="1.5" strokeLinecapp="round" />
+			<circle cx="12" cy="12" r="10" stroke="#1C274C" strokeWidth="1.5" />
+			<path d="M15 12H9" stroke="#1C274C" strokeWidth="1.5" strokelinecapp="round" />
 		</svg>
 	);
 };
