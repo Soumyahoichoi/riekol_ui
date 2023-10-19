0101d780d3acbbaacb62b6ea0eee7f689c1a5956
soumya <soumya.chakraborty@hoichoi.tv>
2023-10-19 16:23:50 +0530

    Analytics add

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router/router';
 import { Toaster } from 'sonner';
-
+import { Analytics } from '@vercel/analytics';
 function App() {
     return (
         <BrowserRouter>
             <Toaster richColors />
             <Router />
+            <Analytics />
         </BrowserRouter>
     );
 }
