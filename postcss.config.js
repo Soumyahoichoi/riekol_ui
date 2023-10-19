1699a9723e929c2f3bd35aa7e0b14195566710ca
Shubhankar Mitra <shubhankar.mitra@hoichoi.tv>
2023-10-19 11:42:47 +0530

    done

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
-// const purgecss = require('@fullhuman/postcss-purgecss');
-
-import purgeCSSPlugin from '@fullhuman/postcss-purgecss';
-
 export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {}
-        // purgecss: purgeCSSPlugin({
-        //     content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js']
-        // })
     }
 };
